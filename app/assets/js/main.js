c7a0870547b3b732e5258a891ac76cd39627e36c
Eduardo Alves <44246348+codespoa@users.noreply.github.com>
2020-06-21 23:32:19 -0300

    Delete main.js

--- app/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function calculate(valueNow) {
-    $('form').submit(function(e) {
-        e.preventDefault();
-        
-        var inputValue = (".convert");
-        alert(inputValue.value)
-    })
-}
